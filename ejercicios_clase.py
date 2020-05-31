5691705ba516a46e93a5057641d3780f1c5ea791
mar-romero <65137011+mar-romero@users.noreply.github.com>
2020-05-31 20:51:35 -0300

    Ejercicios clase
    Hasta el ejercicio 4

--- ejercicios_clase.py
+++ ejercicios_clase.py
@@ -41,7 +41,11 @@ def ej1():
 
     val_max1 = 0
     while x >= val_max1:    # reemplace "condicion" por lo que crea necesario
-        print("Valor de x =", x)
+        if x <= val_max1:
+            print("Valor de x =", x)
+            break
+        x -= 1
+
         
         # Coloque la línea de código para que "X" decremente "1"
 
@@ -51,17 +55,43 @@ def ej2():
 
     # Dado la siguiente lista de colores, utilizar "for"
     # para imprimir en pantalla todos los colores
-    colores = ['rojo', 'naranja', 'verde', 'azul']
-
+    colores = [ 'rojo', 'naranja', 'verde', 'azul' ]
+    
+    for x in colores :
+        if (x == 'rojo') :
+            print (x)
+        if (x == 'naranja'):
+            print (x)
+        if (x == 'verde'):
+            print (x)
+        if (x == 'azul'):
+            print (x)
+        
     # Itere el "for" utilizando la lista como parámero
     # y utilizar como elemento del "for" cada color
     # for color ...
+    # No lo entiendo
 
     # Itere el "for" utilizando el tamaño de la lista
     # como parámetro y utilizar el índice para acceder a
     # los elementos de la lista
     # for i ...
-
+    
+    col_len = len (colores)
+    
+    for i in range(col_len) :
+        if i == 0:
+            col = colores[i]
+            print(col)
+        if i == 1:
+            col = colores[i]
+            print(col)
+        if i == 2:
+            col = colores[i]
+            print(col)
+        if i == 3:
+            col = colores[i]
+            print(col)
 
 def ej3():
     # Ejemplos con bucles "for"
@@ -72,6 +102,9 @@ def ej3():
     numeros = [1, 5, -1, 6, 10, 2, -5]
     suma = 0   # Variable ya inicializada, la suma arranca en cero
 
+    for x in numeros:
+        suma += x
+        print("número = ", x , "sumatoria =", suma)
 
 def ej4():
     # Ejercicios con bucles "while"
@@ -83,13 +116,24 @@ def ej4():
     # una condición compuesta (utilice el operador "and" o "or" según corresponda)
     # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
     # e imprimir en pantalla el resultado de X (antes de incrementar) con print
+    while x < 10 and x !=6:
+        print(x)
+        x += 2
+        print(x)
 
     # Realice el mismo bucle "while" pero en vez de estar formado por una condición
     # compuesta, que el "while" siga iterando mientras <x sea menos a 10>, y dentro del
     # "while" consultar si <x es igual a 6>, y en ese caso realizar una interrupción del bucle
     # En cada iteracion del bucle debe incrementar el valor de "x" en "2"
     # e imprimir en pantalla el resultado de X (antes de incrementar) con print
-
+    y = 0
+    while y < 10 :
+        if y == 6:
+            print('Es igual a 6')
+            break
+        print(y)
+        y += 2
+        print(y)
 
 def ej5():
     # Ejercicio de seuencias numéricas
@@ -130,9 +174,9 @@ def ej6():
 
 if __name__ == '__main__':
     print("Bienvenidos a otra clase de Inove con Python")
-    ej1()
+    #ej1()
     #ej2()
     #ej3()
-    #ej4()
+    ej4()
     #ej5()
     #ej6()
